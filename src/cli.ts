fcc8a4fe57ebe1a59bb1d13ec9f7f9d6625a1d0d
Amit Dahan <amit@neon.tech>
2025-04-10 10:20:12 +0300

    Configure Biome instead of ESLint + Prettier

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import { intro, outro } from "@clack/prompts";
-import { LOGO_LETTERING } from "./lib/art.js";
 import { cristal } from "gradient-string";
+import { LOGO_LETTERING } from "./lib/art.js";
 
 function main() {
 	console.log(cristal(LOGO_LETTERING));
 	intro("Building something cool...");
-	outro(`⭐ and 👀 at github.com/neondatabase/neondb`);
+	outro("⭐ and 👀 at github.com/neondatabase/neondb");
 }
 
 main();
