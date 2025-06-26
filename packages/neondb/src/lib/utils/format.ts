6aacd91e3f3d0ab5cda9bb743eb6c8d57ca26905
Atila Fassina <atila@fassina.eu>
2025-06-26 11:41:32 +0200

    remove unused import

--- packages/neondb/src/lib/utils/format.ts
+++ packages/neondb/src/lib/utils/format.ts
@@ -1,5 +1,3 @@
-import { log } from "@clack/prompts";
-
 export function getPoolerString(connString: string) {
 	const [start, ...end] = connString.split(".");
 	return `${start}-pooler.${end.join(".")}`;
